2bcf2f05614c37560290c7d1f333ea4a9bfafbc6
Axel Ilali <axel.ilali@yahoo.fr>
2023-12-28 18:00:19 +0100

    feat: update plugin version

--- simple-elementor-post-grid.php
+++ simple-elementor-post-grid.php
@@ -8,7 +8,7 @@
  * Author:          Axel Ilali
  * Author URI:      https://axel-ilali.com
  * Text Domain:     simple-elementor-post-grid
- * Version:         1.0.1
+ * Version:         1.0.2
  * Requires at least: 5.8
  * Tested up to: 6.2
  * Requires PHP: 7.3
